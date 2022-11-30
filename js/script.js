70b3182e5245e9ae781aaf91554deb0ed0efaf44
MonicaDeBona <debona.monica@outlook.it>
2022-11-30 16:49:33 +0100

    Create squares inside grid

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-for (let i = 1; i <= 100; i++) {
-    console.log(i);
-}
-
 // Creo dentro al main il div contenitore degli square
 const newMainElement = document.querySelector('main');
 newMainElement.classList.add('d-flex',)
 const newDivContainer = document.createElement('div');
-newDivContainer.classList.add('square','m-auto');
+newDivContainer.classList.add('parent','m-auto', 'd-flex', 'flex-wrap');
 newMainElement.append(newDivContainer);
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+    const newDivSquares = document.createElement('div');
+    newDivSquares.classList.add('square')
+    newDivContainer.append(newDivSquares);
+}
+
+
