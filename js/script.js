d1a5472922b75d714183f6c76a5ab7f2fdc58c76
MonicaDeBona <debona.monica@outlook.it>
2022-11-30 17:29:45 +0100

    Add button play

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
-// Creo dentro al main il div contenitore degli square
-const newMainElement = document.querySelector('main');
-newMainElement.classList.add('d-flex',)
-const newDivContainer = document.createElement('div');
-newDivContainer.classList.add('parent','m-auto', 'd-flex', 'flex-wrap');
-newMainElement.append(newDivContainer);
+const button = document.querySelector('btn');
+button.addEventListener('click', function() {
 
-for (let i = 1; i <= 100; i++) {
-    const newDivSquare = document.createElement('div');
-    newDivSquare.classList.add('square');
+    // Creo dentro al main il div contenitore degli square
+    const newMainElement = document.querySelector('main');
+    newMainElement.classList.add('d-flex',)
+    const newDivContainer = document.createElement('div');
+    newDivContainer.classList.add('parent','m-auto', 'd-flex', 'flex-wrap');
+    newMainElement.append(newDivContainer);
 
-    newDivSquare.addEventListener('click', function() {
-        newDivSquare.classList.toggle('clicked');
-        console.log(i);
-    });
+    // Creo un loop per i numeri da 1 a 100 e dentro creo gli square che diventeranno blu al click mostrando il numero di casella in console.log
+    for (let i = 1; i <= 100; i++) {
+        const newDivSquare = document.createElement('div');
+        newDivSquare.classList.add('square', 'd-flex', 'justify-content-center', 'align-items-center');
+        newDivSquare.innerText = i;
 
-    newDivContainer.append(newDivSquare);
-}
+        newDivSquare.addEventListener('click', function() {
+            newDivSquare.classList.toggle('clicked');
+            console.log(i);
+        });
+
+        newDivContainer.append(newDivSquare);
+    }
+
+});
+
+
+
+
+
 
 
