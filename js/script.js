5399892fde0049ec4016c80c133bd18b8ad34884
MonicaDeBona <debona.monica@outlook.it>
2022-11-30 16:26:41 +0100

    Add classes on created element

--- js/script.js
+++ js/script.js
@@ -4,5 +4,7 @@ for (let i = 1; i <= 100; i++) {
 
 // Creo dentro al main il div contenitore degli square
 const newMainElement = document.querySelector('main');
+newMainElement.classList.add('d-flex',)
 const newDivContainer = document.createElement('div');
+newDivContainer.classList.add('square','m-auto');
 newMainElement.append(newDivContainer);
