12bc2d8a1f99ba8a34cd1fcb9310acf3f3a78f28
MonicaDeBona <debona.monica@outlook.it>
2022-11-30 15:59:29 +0100

    Create div container

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-for (let i = 1; i < 101; i++) {
+for (let i = 1; i <= 100; i++) {
     console.log(i);
 }
+
+// Creo dentro al main il div contenitore degli square
+const newMainElement = document.querySelector('main');
+const newDivContainer = document.createElement('div');
+newMainElement.append(newDivContainer);
