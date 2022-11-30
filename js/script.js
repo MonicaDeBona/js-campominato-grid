1955eb36219782ce8eb51569cb3b996c6505c25d
MonicaDeBona <debona.monica@outlook.it>
2022-11-30 18:29:05 +0100

    Clean function that create element

--- js/script.js
+++ js/script.js
@@ -2,6 +2,36 @@ const button = document.querySelector('div.button a.btn');
 console.log(button);
 
 
+// button.addEventListener('click', function() {
+
+//     // Creo dentro al main il div contenitore degli square
+//     const newMainElement = document.querySelector('main');
+//     newMainElement.innerHTML = ('');
+//     newMainElement.classList.add('d-flex');
+//     const newDivContainer = document.createElement('div');
+//     newDivContainer.classList.add('parent','m-auto', 'd-flex', 'flex-wrap');
+//     newMainElement.append(newDivContainer);
+
+//     // Creo un loop per i numeri da 1 a 100 e dentro creo gli square che diventeranno blu al click mostrando il numero di casella in console.log
+//     for (let i = 1; i <= 100; i++) {
+//         const newDivSquare = getNewDivElement(i);
+//         newDivSquare.addEventListener('click', function() {
+//             newDivSquare.classList.toggle('clicked');
+//             console.log(i);
+//         });
+//         newDivContainer.append(newDivSquare);
+//     }
+// });
+
+// function getNewDivElement(content) {
+//     const newDivSquare = document.createElement('div');
+//     newDivSquare.classList.add('square', 'd-flex', 'justify-content-center', 'align-items-center');
+//     newDivSquare.innerText = content;
+
+//     return newDivSquare;
+// }
+
+
 button.addEventListener('click', function() {
 
     // Creo dentro al main il div contenitore degli square
@@ -14,7 +44,9 @@ button.addEventListener('click', function() {
 
     // Creo un loop per i numeri da 1 a 100 e dentro creo gli square che diventeranno blu al click mostrando il numero di casella in console.log
     for (let i = 1; i <= 100; i++) {
-        const newDivSquare = getNewDivElement(i);
+
+        const newDivSquare = getNewElement('div', i, 'square', 'd-flex', 'justify-content-center', 'align-items-center');
+
         newDivSquare.addEventListener('click', function() {
             newDivSquare.classList.toggle('clicked');
             console.log(i);
@@ -23,9 +55,9 @@ button.addEventListener('click', function() {
     }
 });
 
-function getNewDivElement(content) {
-    const newDivSquare = document.createElement('div');
-    newDivSquare.classList.add('square', 'd-flex', 'justify-content-center', 'align-items-center');
+function getNewElement(elementTag, content, classesToAdd) {
+    const newDivSquare = document.createElement(elementTag);
+    newDivSquare.classList.add(classesToAdd);
     newDivSquare.innerText = content;
 
     return newDivSquare;
